fa084e8b98c53183d5f182540d10cff6a57ecd55
Yewon Yoo <yooyy0106@naver.com>
2023-08-24 18:21:17 +0900

    마이페이지, 시간표

--- app/src/main/java/com/example/sdukdy/SubjectActivity.kt
+++ app/src/main/java/com/example/sdukdy/SubjectActivity.kt
@@ -1,11 +1,20 @@
 package com.example.sdukdy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SubjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_subject)
+
+        findViewById<View>(R.id.o_subject_add1).setOnClickListener {
+            val intent = Intent(this@SubjectActivity, TimeTableActivity::class.java)
+            intent.putExtra("color", android.graphics.Color.RED)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/sdukdy/TimeTableActivity.kt
+++ app/src/main/java/com/example/sdukdy/TimeTableActivity.kt
@@ -1,11 +1,48 @@
 package com.example.sdukdy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class TimeTableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_table)
+
+        findViewById<View>(R.id.o_timetable_addBtn).setOnClickListener {
+            //SubjectAcitivy로 이동 (과목 추가하기 버튼)
+            val intent = Intent(this@TimeTableActivity, SubjectActivity::class.java)
+            startActivity(intent)
+        }
+
+        //하단바 동작
+        findViewById<View>(R.id.o_timetable_homeicon).setOnClickListener {
+            //HomeAcitivy로 이동
+            val intent = Intent(this@TimeTableActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<View>(R.id.o_timetable_chatting).setOnClickListener {
+            //ChattingAcitivy로 이동
+            val intent = Intent(this@TimeTableActivity, ChattingActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<View>(R.id.o_timetable_mypage).setOnClickListener {
+            //MypageAcitivy로 이동
+            val intent = Intent(this@TimeTableActivity, MypageActivity::class.java)
+            startActivity(intent)
+        }
+
+        val targetCell: TextView = findViewById(R.id.monday09)
+
+        // MainActivity에서 전달된 색상을 가져와 설정
+        val color = intent.getIntExtra("color", 0)
+        if (color != 0) {
+            targetCell.setBackgroundColor(color)
+        }
+
     }
 }
